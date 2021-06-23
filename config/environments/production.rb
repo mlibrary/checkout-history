b587a8f1483e8689d6ac6b6965233be774b39802
Monique Rio <mrio@umich.edu>
2021-06-23 10:26:05 -0400

    logs to stdout and using dev_db for here

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,9 +82,9 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    $stdout.sync = true
+    config.rails_semantic_logger.add_file_appender = false
+    config.semantic_logger.add_appender(io: $stdout, level: config.log_level, formatter: config.rails_semantic_logger.format)
   end
 
   # Do not dump schema after migrations.
